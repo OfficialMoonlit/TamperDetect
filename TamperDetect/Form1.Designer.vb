ee5a75b4e6cc585c8edc4cf41d7b91a368245990
Moonlit <mltgit@live.co.uk>
2012-12-02 11:15:03 +0000

    Version 1.0

---
+++ TamperDetect/Form1.Designer.vb
@@ -0,0 +1,102 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.lblAlert = New System.Windows.Forms.Label()
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.lblState = New System.Windows.Forms.Label()
+        Me.tmrCheck = New System.Windows.Forms.Timer(Me.components)
+        Me.lblTPS = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblAlert
+        '
+        Me.lblAlert.BackColor = System.Drawing.Color.Lime
+        Me.lblAlert.Location = New System.Drawing.Point(12, 9)
+        Me.lblAlert.Name = "lblAlert"
+        Me.lblAlert.Size = New System.Drawing.Size(64, 64)
+        Me.lblAlert.TabIndex = 0
+        '
+        'lblStatus
+        '
+        Me.lblStatus.AutoSize = True
+        Me.lblStatus.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblStatus.ForeColor = System.Drawing.Color.White
+        Me.lblStatus.Location = New System.Drawing.Point(82, 9)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(45, 15)
+        Me.lblStatus.TabIndex = 1
+        Me.lblStatus.Text = "Status:"
+        '
+        'lblState
+        '
+        Me.lblState.AutoSize = True
+        Me.lblState.Font = New System.Drawing.Font("Arial", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblState.ForeColor = System.Drawing.Color.White
+        Me.lblState.Location = New System.Drawing.Point(82, 24)
+        Me.lblState.Name = "lblState"
+        Me.lblState.Size = New System.Drawing.Size(170, 56)
+        Me.lblState.TabIndex = 2
+        Me.lblState.Text = "GOOD"
+        '
+        'tmrCheck
+        '
+        Me.tmrCheck.Enabled = True
+        Me.tmrCheck.Interval = 10
+        '
+        'lblTPS
+        '
+        Me.lblTPS.BackColor = System.Drawing.Color.Lime
+        Me.lblTPS.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTPS.ForeColor = System.Drawing.Color.Black
+        Me.lblTPS.Location = New System.Drawing.Point(12, 49)
+        Me.lblTPS.Name = "lblTPS"
+        Me.lblTPS.Size = New System.Drawing.Size(64, 24)
+        Me.lblTPS.TabIndex = 3
+        Me.lblTPS.Text = "0/s"
+        Me.lblTPS.TextAlign = System.Drawing.ContentAlignment.BottomRight
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(277, 83)
+        Me.Controls.Add(Me.lblTPS)
+        Me.Controls.Add(Me.lblState)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.lblAlert)
+        Me.Name = "frmMain"
+        Me.Text = "Keyboard Tamper Detect"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblAlert As System.Windows.Forms.Label
+    Friend WithEvents lblStatus As System.Windows.Forms.Label
+    Friend WithEvents lblState As System.Windows.Forms.Label
+    Friend WithEvents tmrCheck As System.Windows.Forms.Timer
+    Friend WithEvents lblTPS As System.Windows.Forms.Label
+
+End Class
---
+++ TamperDetect/Form1.vb
@@ -0,0 +1,42 @@
+﻿Public Class frmMain
+
+    Dim curCapsLckState As Boolean
+    Dim curScrLckState As Boolean
+    Dim curNumLckState As Boolean
+    Dim curTick As Integer
+    Dim curToggleCount
+    Dim countDuration As Integer = 100
+    Dim toggleThreshold As Integer = 5
+    Dim toggleCount As Integer
+    Dim maxTPS As Integer
+
+    Private Sub tmrCheck_Tick(sender As System.Object, e As System.EventArgs) Handles tmrCheck.Tick
+        If curTick >= countDuration Then
+            curTick = 0
+            toggleCount = 0
+        End If
+
+        If IsKeyLocked(Keys.CapsLock) <> curCapsLckState Then toggleCount = toggleCount + 1
+        If IsKeyLocked(Keys.Scroll) <> curScrLckState Then toggleCount = toggleCount + 1
+        If IsKeyLocked(Keys.NumLock) <> curNumLckState Then toggleCount = toggleCount + 1
+
+        curCapsLckState = IsKeyLocked(Keys.CapsLock)
+        curScrLckState = IsKeyLocked(Keys.Scroll)
+        curNumLckState = IsKeyLocked(Keys.NumLock)
+
+        If toggleCount >= toggleThreshold Then
+            lblAlert.BackColor = Color.Red
+            lblTPS.BackColor = Color.Red
+            lblTPS.ForeColor = Color.White
+            lblState.Text = "BAD"
+        End If
+
+        If toggleCount > maxTPS Then
+            maxTPS = toggleCount
+            lblTPS.Text = maxTPS.ToString & "/s"
+        End If
+
+        curTick = curTick + 1
+    End Sub
+
+End Class
